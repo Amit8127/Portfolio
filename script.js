29de7c2d09476b761bbaef3f1c7b52b7d2cc2f33
Amit8127 <amitmonu0000@gmail.com>
2023-12-23 23:42:42 +0530

    Update Photo anf form section

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ var typed = new Typed(".text", {
   backDelay: 1000,
   loop: true,
 });
+
+function sendMail() {
+  (function(){
+    emailjs.init("Qxpkn8FQ_ppfr09OU")
+  })();
+
+  var params = {
+    sendername: document.querySelector("#sendername").value,
+    from: document.querySelector("#from").value,
+    subject: document.querySelector("#subject").value,
+    message: document.querySelector("#message").value,
+  };
+
+  var serviceId = "service_nxs163z";
+  var templateId = "template_e63xzwn";
+
+  emailjs.send(serviceId, templateId, params)
+  .catch();
+  document.getElementById("myForm").reset();
+}
